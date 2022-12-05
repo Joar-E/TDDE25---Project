9b9aacc0cd0f05f4c71510eae4cbd5c380214da1
Felix Gustafsson Ahlberg <felgu205@student.liu.se>
2022-12-05 14:41:43 +0000

    Sounds-folder uploaded

---
+++ ctf-master/sounds.py
@@ -0,0 +1,20 @@
+import pygame
+import os
+
+
+
+pygame.mixer.init()
+
+flag_sound = pygame.mixer.Sound("data/flag_sound.wav")
+
+shoot_sound = pygame.mixer.Sound("data/tank_shoot_sound.wav")
+
+box_sound = pygame.mixer.Sound("data/box_sound.wav")
+
+background_music = pygame.mixer.Sound("data/background_music.wav")
+
+tank_shot_sound = pygame.mixer.Sound("data/tank_shot_sound.wav")
+
+
+
+
